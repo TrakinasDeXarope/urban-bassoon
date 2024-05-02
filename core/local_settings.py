44d6defe625b8c59ef69d2cbea823fd2e504c802
TrakinasDeXarope <165822198+TrakinasDeXarope@users.noreply.github.com>
2024-05-02 13:31:15 -0300

    Add files via upload

---
+++ core/local_settings.py
@@ -0,0 +1,17 @@
+# SECRET_KEY = 'o[BBGI"=Vbrb:5UqKLIh}~<7ok;%}}vt?eLBB\A#3l"$wSQl@!\Z/Ha0/JbV&tx.'
+
+# # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = False
+
+# ALLOWED_HOSTS = ['*']
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'projeto_agenda',
+#         'USER': 'joao',
+#         'PASSWORD': '05101810',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
